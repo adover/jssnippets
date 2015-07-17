49c20307ac4c2a3e884a43daf5ad8c5eca2cf623
Andrew Dover <andrew_dover@hotmail.com>
2015-07-17 17:49:22 +1200

    added checking for up or down states

---
+++ upordownchecking.js
@@ -0,0 +1,43 @@
+      $(document,window).on("keydown keyup", function(e) {
+          e.preventDefault();
+          console.log(e.which)
+          // Space is 32
+          // page up 33
+          // page down 34
+          // end 35
+          // home  36
+          // up arrow  38
+          // down arrow  40
+          // tab 9
+
+          if(e.which == 32 || e.which == 34 || e.which == 35 || e.which == 40){
+            utils.vector = "down";
+          }else if(e.which == 33 || e.which == 36 || e.which == 38){
+            utils.vector = "up";
+          }else if(e.which == 9){
+            return false;
+          }
+
+      });
+    
+      $(window).swipe({
+        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+          if(direction == "up"){
+            utils.vector = "up";
+          }else if(direction == "down"){
+            utils.vector = "down";
+          }
+        }
+      })
+
+      $(window).mousewheel(function(event, delta){
+
+        if(delta > 0){
+          // Scrolling up
+          utils.vector = "up";
+        }else{
+          // Scrolling down
+          utils.vector = "down";
+        }
+
+      });
